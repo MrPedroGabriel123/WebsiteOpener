bbd2f86f1a741fc3b6dbcf878ce5c85bba90ff41
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-02-27 20:30:15 +0000

    full commit

--- MentalIllness/Program.cs
+++ MentalIllness/Program.cs
@@ -1,12 +1,16 @@
-﻿namespace Websiteopener;
+﻿using System.Diagnostics;
+using System.Security;
+using System.ComponentModel;
+namespace Websiteopener;
 
 internal static class Webopener
 {
     public static void Main()
     {
-            Console.ForegroundColor = ConsoleColor.Green;
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("Welcome!!");
         Console.WriteLine("In this program you can open your favorite website in one click :D");
+       
         while (true) { 
             
             Console.WriteLine("Please choose one website from the list below");
@@ -23,23 +27,43 @@ internal static class Webopener
 
             if (userFirstChoice == "gamesense")
             {
+                var gamesense = "https://gamesense.pub/forums/";
+                var psi = new System.Diagnostics.ProcessStartInfo();
+                psi.UseShellExecute = true;
+                psi.FileName = gamesense;
+                System.Diagnostics.Process.Start(psi);
                 System.Diagnostics.Process.Start($"https://gamesense.pub/forums/");
                 Console.WriteLine($"Opening {userFirstChoice}");
             }
             if (userFirstChoice == "youtube")
             {
+                var youtube = "https://www.youtube.com/";
+                var psi = new System.Diagnostics.ProcessStartInfo();
+                psi.UseShellExecute = true;
+                psi.FileName = youtube;
+                System.Diagnostics.Process.Start(psi);
+
                 System.Diagnostics.Process.Start($"https://www.youtube.com/");
                 Console.WriteLine($"Opening {userFirstChoice}");
             }
 
             if (userFirstChoice == "google")
             {
+                var google = "https://www.google.pt/";
+                var psi = new System.Diagnostics.ProcessStartInfo();
+                psi.UseShellExecute = true;
+                psi.FileName = google;
+
                 System.Diagnostics.Process.Start($"https://www.google.pt/");
                 Console.WriteLine($"Opening {userFirstChoice}");
             }
-
+                
             if (userFirstChoice == "twitch")
             {
+                var twitch = "https://www.twitch.tv/";
+                var psi = new System.Diagnostics.ProcessStartInfo();
+                psi.UseShellExecute = true;
+                psi.FileName = twitch;
                 System.Diagnostics.Process.Start($"https://www.twitch.tv/");
                 Console.WriteLine($"Opening {userFirstChoice}");
             }
