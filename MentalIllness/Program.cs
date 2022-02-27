4f3d4793bf09b310667842a90871b40c939eed56
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-02-27 16:10:48 +0000

    1

--- MentalIllness/Program.cs
+++ MentalIllness/Program.cs
@@ -4,12 +4,12 @@ internal static class Webopener
 {
     private static void Main()
     {
-        Console.ForegroundColor = ConsoleColor.Red;
-       
-           
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Welcome!!");
             Console.WriteLine("In this program you can open your favorite website in one click :D");
             Console.WriteLine("Please choose one website from the list below");
-        
+       
+            
+
     }
 }
