35d07e49c0644e1f8304683836ac4ffcac3deffe
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-02-27 16:21:10 +0000

    second update

--- MentalIllness/Program.cs
+++ MentalIllness/Program.cs
@@ -2,14 +2,22 @@
 
 internal static class Webopener
 {
-    private static void Main()
+    public static void Main()
     {
             Console.ForegroundColor = ConsoleColor.Green;
+        while (true) { 
             Console.WriteLine("Welcome!!");
             Console.WriteLine("In this program you can open your favorite website in one click :D");
             Console.WriteLine("Please choose one website from the list below");
-       
-            
+            Console.WriteLine("gamesense");
+            Console.WriteLine("youtube");
+            Console.WriteLine("twitch");
+            var userFirstChoice = Console.ReadLine();
+             if (!int.TryParse(userFirstChoice, out int choice))
+        {
+            Console.WriteLine("Invalid website...");
+        }
 
+        }
     }
 }
