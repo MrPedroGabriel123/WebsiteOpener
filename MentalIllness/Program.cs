78d09c30c2c12692c8564cb526e23467a7f102c7
MrPedroGabriel123 <joaofelipechannel0@gmail.com>
2022-02-27 16:56:07 +0000

    test

--- MentalIllness/Program.cs
+++ MentalIllness/Program.cs
@@ -5,18 +5,33 @@ internal static class Webopener
     public static void Main()
     {
             Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Welcome!!");
+        Console.WriteLine("In this program you can open your favorite website in one click :D");
         while (true) { 
-            Console.WriteLine("Welcome!!");
-            Console.WriteLine("In this program you can open your favorite website in one click :D");
+            
             Console.WriteLine("Please choose one website from the list below");
             Console.WriteLine("gamesense");
             Console.WriteLine("youtube");
             Console.WriteLine("twitch");
             var userFirstChoice = Console.ReadLine();
-             if (!int.TryParse(userFirstChoice, out int choice))
-        {
-            Console.WriteLine("Invalid website...");
-        }
+            if (string.IsNullOrEmpty(userFirstChoice)) 
+            {
+                Console.WriteLine("Invalid website...");
+                continue;
+            }
+
+            if (userFirstChoice == "gamesense")
+            {
+                System.Diagnostics.Process.Start(@"https://gamesense.pub/forums/");
+            }
+
+
+
+
+
+
+
+
 
         }
     }
